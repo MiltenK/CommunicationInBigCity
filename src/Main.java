25b48c4c7485f30054db7dafac6a0a9d8901a4c9
MiltenK <dmitry.a.kupriyanov@gmail.com>
2026-05-16 15:03:33 +0300

    Refactoring

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,38 @@ public class Main {
         System.out.println("Введите кол-во станций");
         Scanner sc = new Scanner(System.in);
         int stationCount = Integer.parseInt(sc.nextLine());
+        ArrayList<Station> stationsArr = getStations(stationCount, sc);
+
+        Point result = getResult(sc);
+
+        ArrayList<Station>  goodStations = new ArrayList<>();
+        for (int i = 0; i < stationsArr.size(); i++) {
+            if (checkIfPos(result, stationsArr.get(i))){
+                goodStations.add(stationsArr.get(i));
+            }
+        }
+        System.out.println(goodStations);
+
+
+
+    }
+
+
+    private static Point getResult(Scanner sc) {
+        System.out.println("Введите ваши координаты");
+        String lineOurCoor = sc.nextLine();
+        String[] arrOurCoor = lineOurCoor.split(" ");
+        int ourX = Integer.parseInt(arrOurCoor[0]);
+        int ourY = Integer.parseInt(arrOurCoor[1]);
+        Point result = new Point(ourX, ourY);
+        return result;
+    }
+
+
+
+
+
+    private static ArrayList<Station> getStations(int stationCount, Scanner sc) {
         ArrayList<Station> stationsArr = new ArrayList<>();
         System.out.println("Введите станции");
 
@@ -19,26 +51,10 @@ public class Main {
             Station station = new Station(stationName, x, y, r);
             stationsArr.add(station);
         }
-
-        System.out.println("Введите ваши координаты");
-        String lineOurCoor = sc.nextLine();
-        String[] arrOurCoor = lineOurCoor.split(" ");
-        int ourX = Integer.parseInt(arrOurCoor[0]);
-        int ourY = Integer.parseInt(arrOurCoor[1]);
-
-        ArrayList<Station>  goodStations = new ArrayList<>();
-        for (int i = 0; i < stationsArr.size(); i++) {
-            if (checkIfPos(ourX, ourY, stationsArr.get(i))){
-                goodStations.add(stationsArr.get(i));
-            }
-        }
-        System.out.println(goodStations);
-
-
-
+        return stationsArr;
     }
 
-    public static boolean checkIfPos(int ourX, int ourY, Station stations){
-        return stations.r >= stations.point.distanceTo(new Point(ourX, ourY));
+    public static boolean checkIfPos(Point p, Station stations){
+        return stations.r >= stations.point.distanceTo(p);
     }
 }
